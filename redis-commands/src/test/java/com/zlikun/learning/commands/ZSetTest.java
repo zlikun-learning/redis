fd4801b1479183ffe63def24ec681fc0beb6491d
zlikun <zlikun-dev@hotmail.com>
2017-09-18 11:35:06 +0800

    测试ZSET交集、并集运算

--- redis-commands/src/test/java/com/zlikun/learning/commands/ZSetTest.java
+++ redis-commands/src/test/java/com/zlikun/learning/commands/ZSetTest.java
@@ -20,15 +20,17 @@ import redis.clients.jedis.Tuple;
 public class ZSetTest extends TestBase {
 
 	private String key = "zlikun:zset" ;
+	private String key2 = "zlikun:zset2" ;
+	private String key3 = "zlikun:zset2" ;
 	
 	@Override
 	protected void init() {
-		jedis.del(key) ;
+		jedis.del(key ,key2 ,key3) ;
 	}
 	
 	@Override
 	protected void destroy() {
-		jedis.del(key) ;
+		jedis.del(key ,key2 ,key3) ;
 	}
 	
 	@Test
@@ -105,6 +107,59 @@ public class ZSetTest extends TestBase {
 		// 删除元素(指定词典区间)
 		Assert.assertEquals(Long.valueOf(0), jedis.zremrangeByLex(key, "-", "+")) ;
 		
+	}
+	
+	@Test
+	public void zinterstore() {
+		
+		// 准备数据
+		jedis.zadd(key, 98.0, "zlikun") ;
+		jedis.zadd(key, 67.5, "nginx") ;
+		jedis.zadd(key, 62.0, "apache") ;
+		
+		jedis.zadd(key2, 92.0, "zlikun") ;
+		jedis.zadd(key2, 48.5, "nodejs") ;
+		
+		// 计算多个有序集合交集，将结果写入目标有序集合中
+		Assert.assertEquals(Long.valueOf(1), jedis.zinterstore(key3, key ,key2)) ;
+		// 从结果得出结论：交集运算时，成员是交集条件，分数取相交成员分数之和
+		// zlikun/190.0
+		log.info("{}" ,format(jedis.zrangeWithScores(key3, 0, -1)));
+		
+		// 从输出结果发现，key2中不相交的成员被删除了
+		log.info("{}" ,jedis.zrange(key, 0, -1));
+		log.info("{}" ,jedis.zrange(key2, 0, -1));
+		
+	}
+
+	@Test
+	public void zunionstore() {
+		
+		// 准备数据
+		jedis.zadd(key, 98.0, "zlikun") ;
+		jedis.zadd(key, 67.5, "nginx") ;
+		jedis.zadd(key, 62.0, "apache") ;
+		
+		jedis.zadd(key2, 92.0, "zlikun") ;
+		jedis.zadd(key2, 48.5, "nodejs") ;
+		
+		// 计算多个有序集合并集，将结果写入目标有序集合中
+		Assert.assertEquals(Long.valueOf(4), jedis.zunionstore(key3, key ,key2)) ;
+		// 从结果得出结论：并集运算时，成员是交集条件，分数取相交成员分数之和 
+		// nodejs/48.5,apache/62.0,nginx/67.5,zlikun/190.0
+		log.info("{}" ,format(jedis.zrangeWithScores(key3, 0, -1)));
+		
+		// 从输出结果发现，key中并集中多出的成员被加入了key2
+		log.info("{}" ,jedis.zrange(key, 0, -1));
+		log.info("{}" ,jedis.zrange(key2, 0, -1));
+	}
+	
+	private String format(Set<Tuple> result) {
+		StringBuilder builder = new StringBuilder() ;
+		for(Tuple tuple : result) {
+			builder.append(tuple.getElement()).append("/").append(tuple.getScore()).append(",") ;
+		}
+		return builder.substring(0 ,builder.length() - 1) ;
 	}
 	
 }
